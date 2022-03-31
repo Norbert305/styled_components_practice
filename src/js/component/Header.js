4b9dfba8ef489f95adfccfafaa76d2856de76652
Norbert305 <sealsnorbert@yahoo.com>
2022-03-31 21:26:29 +0000

    updates

--- src/js/component/Header.js
+++ src/js/component/Header.js
@@ -1,10 +1,35 @@
 import React from "react";
-import { StyledHeader } from "./styled/Header.styled";
+import { StyledHeader, Nav, Logo } from "./styled/Header.styled";
+import { Container } from "./styled/Container.styled";
+import { Button } from "./styled/Button.styled";
+import { Flex } from "./styled/Flex.styled";
 
 export const Header = () => {
 	return (
 		<StyledHeader>
-			<h1>Hubble</h1>
+			<Container>
+				<Nav>
+					<Logo
+						src="https://www.looper.com/img/gallery/heres-how-long-keanu-reeves-said-hell-play-john-wick-for/intro-1598907086.jpg"
+						alt=""
+					/>
+					<Button>Click Me</Button>
+				</Nav>
+				<Flex>
+					<div>
+						<h1>John Whick can't be stopped!</h1>
+						<p>
+							Hello World. This is my styled compenent projcet
+							that I am working on for fun!!! John Whick is
+							unbeatable!!!!!
+						</p>
+
+						<button bg="#ff0099" color="#fff">
+							Getting Started For Free
+						</button>
+					</div>
+				</Flex>
+			</Container>
 		</StyledHeader>
 	);
 };
--- src/js/component/styled/Header.styled.js
+++ src/js/component/styled/Header.styled.js
@@ -11,3 +11,12 @@ export const StyledHeader = styled.header`
 		background-color: black;
 	}
 `;
+
+export const Nav = styled.nav`
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	margin-bottom: 40px;
+`;
+
+export const Logo = styled.img``;
